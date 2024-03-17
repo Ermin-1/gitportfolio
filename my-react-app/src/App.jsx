b1a9df93cd816d30c037dd930e81b15de8000e04
Ermin Husic <Ermin.Husic@live.se>
2024-03-17 21:23:32 +0100

    second  commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.jsx
+import React, { useState } from 'react';
+import Portfolio from './Portfolio'; // Importera Portfolio-komponenten
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showProjects, setShowProjects] = useState(false);
+
+  const handleShowProjects = () => {
+    setShowProjects(true);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+<div className="app-wrapper">
+  <header>
+    <h1></h1>
+  </header>
+  <main className="main-content">
+    <div className="container">
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="card-body">
+          <h5 className="card-title">Ermins Portfolio API</h5>
+          <button className="btn btn-primary" onClick={handleShowProjects}>Visa GitHub-projekt</button>
+          {showProjects && <Portfolio />} {/* Visa Portfolio-komponenten om showProjects är true */}
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  </main>
+  <footer>
+    <p></p>
+  </footer>
+</div>
+
+  );
 }
 
-export default App
+export default App;
